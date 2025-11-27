372abe38db976223f0104f90ab0a502acecc024f
Mark Meshcheryakov <markzum@yandex.ru>
2025-11-27 10:57:38 +0300

    Removed last changes

--- server/tools/common.py
+++ server/tools/common.py
@@ -1,10 +1,7 @@
 from langchain.tools import tool
 
-from .conversation import deliver_user_answer, request_user_clarification
 from .events import get_events
 
 tools = [
 	tool(get_events),
-	request_user_clarification,
-	deliver_user_answer,
 ]
