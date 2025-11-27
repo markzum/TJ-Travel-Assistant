43311df191f6a2004fe0790a9a213a8905966b0d
Mark Meshcheryakov <markzum@yandex.ru>
2025-11-27 11:18:24 +0300

    Bug fixed

--- server/tools/__init__.py
+++ server/tools/__init__.py
@@ -1,4 +1,3 @@
 from .events import get_events
-from .conversation import request_user_clarification, deliver_user_answer
 
-__all__ = ['get_events', 'request_user_clarification', 'deliver_user_answer']
+__all__ = ['get_events']
